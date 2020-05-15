cafaf1eb7b787c81c121414fba3f00ab3a067220
aymenbne548485 <57303307+aymenbne548485@users.noreply.github.com>
2020-05-15 20:01:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,6 +104,52 @@ client.on('message', message => {
 
 
 
+client.on("message", message => {
+
+    if (message.content.startsWith(prefix + "bcall")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : Messages received`); 
+message.delete(); 
+};     
+});
+
+
+client.on("message", message => {
+
+    if (message.content.startsWith(prefix + "bcoff")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : Messages received`); 
+message.delete(); 
+};     
+});
+
+
+client.on("message", message => {
+
+  if (message.content.startsWith(prefix + "bcon")) {
+               if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'online').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'offline').size}\` : عدد الاعضاء المستلمين`); 
+message.delete(); 
+};     
+});
+
+
+
 
 
 
