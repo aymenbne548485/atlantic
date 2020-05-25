1bb836ec0a86fa1c1c3910b6f95658d8a528b4e4
aymenbne548485 <57303307+aymenbne548485@users.noreply.github.com>
2020-05-25 15:25:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,32 +104,6 @@ client.on('message', message => {
 
 
 
-client.on("message", message => {
-
-    if (message.content.startsWith(prefix + "bcall")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-let args = message.content.split(" ").slice(1);
-var argresult = args.join(' '); 
-message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-m.send(`${argresult}\n ${m}`);
-})
-message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : Messages received`); 
-message.delete(); 
-};     
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
